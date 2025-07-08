de93752a668bb90bdf2d45216262558f2d8d1ae1
CloudCante <cloudhulu@gmail.com>
2025-07-08 11:42:30 -0400

    changed it to connect to local host on the VM to see if it changes the outcome.

--- backend/db.js
+++ backend/db.js
@@ -6,14 +6,14 @@ console.log('Initializing database configuration...');
 dotenv.config();
 
 console.log('Creating database pool with config:', {
-    host: '10.23.8.41',
+    host: 'localhost',
     port: 5432,
     database: 'fox_db',
     user: 'gpu_user'
 });
 
 const pool = new Pool({
-    host: '10.23.8.41',
+    host: 'localhost',
     port: 5432,
     database: 'fox_db',
     user: 'gpu_user',
